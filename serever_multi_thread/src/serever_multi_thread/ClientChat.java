407dea4b961860dc0226cd07f242d4d2404c0911
OUBRAYME-MUSTAFA <102171461+OUBRAYME-MUSTAFA@users.noreply.github.com>
2022-03-25 00:26:03 +0100

    Update ClientChat.java

--- serever_multi_thread/src/serever_multi_thread/ClientChat.java
+++ serever_multi_thread/src/serever_multi_thread/ClientChat.java
@@ -1,18 +1,34 @@
 package serever_multi_thread;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
 public class ClientChat extends Application{
 	
+	PrintWriter pw ;
  public static void main(String[] arg) {
 	 launch(arg);
  }
@@ -29,14 +45,68 @@ public void start(Stage primaryStage) throws Exception {
 	Label     labebPort = new Label("Port : ");
 	TextField text2     = new TextField("3456");
 	
-	Button    Butt1     = new Button("connecter");
+	Button    buttonConnecter     = new Button("connecter");
 	
 	HBox   hbox = new HBox(); hbox.setSpacing(10); hbox.setPadding(new Insets(10));
-	hbox.getChildren().addAll(labebHost,text1,labebPort,text2,Butt1);
+	hbox.setBackground(new Background(new BackgroundFill(Color.ORANGE,null,null)));
+	hbox.getChildren().addAll(labebHost,text1,labebPort,text2,buttonConnecter);
+	
+	
+	
+	VBox   vbox = new VBox(); vbox.setSpacing(10); vbox.setPadding(new Insets(10));
+	ObservableList<String> listModel = FXCollections.observableArrayList();
+	ListView<String> listView = new ListView<String>(listModel);
+	vbox.getChildren().add(listView);
+	
+	Label labelMessage = new Label("Message");
+	TextField textFieldMessage = new TextField();
+	textFieldMessage.setPrefSize(300, 30);
+	Button buttonEnvoyer = new Button("Envoyer");
+	
+	HBox hBox2 = new HBox();
+	hBox2.setSpacing(10);
+	hBox2.setPadding(new Insets(10));
+	hBox2.getChildren().addAll(labelMessage, textFieldMessage, buttonEnvoyer);
 	
 	B1.setTop(hbox);
+	B1.setCenter(vbox);
+	B1.setBottom(hBox2);
 	
 	Scene scene = new Scene(B1,500,400);
+	primaryStage.setScene(scene);
 	primaryStage.show();
+	
+	buttonConnecter.setOnAction((evt)->{
+		String host = text1.getText();
+		int port = Integer.parseInt(text2.getText());
+		try {
+			Socket socket = new Socket(host, port);
+			InputStream is = socket.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			pw = new PrintWriter(socket.getOutputStream(), true);
+			new Thread(()->{
+				while(true) {
+					try {							
+						String response = br.readLine();
+						Platform.runLater(()->{
+							listModel.add(response);
+						});
+					}catch (IOException e) {							
+						e.printStackTrace();
+					}
+				}
+			}).start();
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	});
+	
+	buttonEnvoyer.setOnAction((evt)->{
+		String msg = textFieldMessage.getText();
+		pw.println(msg);
+	});
 }
 }
